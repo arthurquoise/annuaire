7e618d0a1a87a23ab237e71526f6825d859f88e4
Arthur <arthur.dennetiere@gmail.com>
2021-12-15 09:56:50 +0100

    Init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace annuaire.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
