32d1d1645755ce0ec7148a7e1f7dad92295f8cbe
Arthur <arthur.dennetiere@gmail.com>
2022-01-09 14:44:38 +0100

    feat:hash password

--- Models/UserLogin.cs
+++ Models/UserLogin.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
 
 namespace annuaire.Models
 {
@@ -24,6 +27,7 @@ namespace annuaire.Models
         public static bool CheckAuthentification(UserLogin user)
         {
             int result = 0;
+            user.password = HashPassword(user.password);
             request = "SELECT COUNT(*) FROM user_login WHERE user_name = @UserName AND user_password = @UserPassword";
             connection = Db.Connection;
             command = new MySqlCommand(request, connection);
@@ -43,6 +47,19 @@ namespace annuaire.Models
 
             return result == 1;
 
+        }
+
+        private static string HashPassword(string password)
+        {
+            using MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
         }
     }
 }
